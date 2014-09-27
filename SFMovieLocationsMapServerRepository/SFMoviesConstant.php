cf60c06ead28fbfca88753bf305fb958b2e84e4b
Jayesh Kawli <j.kawli@gmail.com>
2014-09-27 18:53:07 -0400

    Updated database invalidation time from 7 hours to 1 week

--- SFMovieLocationsMapServerRepository/SFMoviesConstant.php
+++ SFMovieLocationsMapServerRepository/SFMoviesConstant.php
@@ -5,6 +5,6 @@ define("SFMoviesBaseURL", "https://data.sfgov.org");
 define("SFMoviesAPIVersion", "resource");
 define("SFMoviesRequestAPIToken", "2VCGQ8tU4XpEmeLsjBtKC2iOT");
 define("SFMoviesDatabaseUpdateStorageFile", "lastDatabaseUpdateTimestampFile.txt");
-$number_of_approximate_seconds_in_week=25000;
+$number_of_approximate_seconds_in_week=604800;
 
 ?>
